9fb25a67a46ea2d0d75b0d7e945cb7d604d30109
defnullx <defnullx@outlook.com>
2022-05-08 00:58:27 -0300

    update and add new file

--- scraptradeconomics.py
+++ scraptradeconomics.py
@@ -6,20 +6,21 @@ csvfile01 = "tbl01"
 csvfile02 = "tbl02"
 csvfile03 = "tbl03"
 
+
 class scraptbl(scrapy.Spider):
     name = "scraptbl"
-    start_urls = ['https://tradingeconomics.com/country-list/interest-rate?continent=world']
+    start_urls = [
+        'https://tradingeconomics.com/country-list/interest-rate?continent=world']
 
     def parse(self, response):
-        #link = response.xpath('//a[contains(@class, "next-posts-link")]/a/@href').get()  # extract using class
-        #response.xpath('')
-        for sel in response.xpath('//div[@id="ctl00_ContentPlaceHolder1_ctl02_UpdatePanel1"]//div[@class="panel panel-default"]//div[@class="table-responsive"]//table[@class="table table-hover table-heatmap"]//tr//td/a'):
-            country = sel.xpath('@href').get()
+        tableheader = '//div[@id="ctl00_ContentPlaceHolder1_ctl02_UpdatePanel1"]//div[@class="panel panel-default"]//div[@class="table-responsive"]//table[@class="table table-hover table-heatmap"]//tr//td/a[last()]'
+        tablevalues = '//div[@id="ctl00_ContentPlaceHolder1_ctl02_UpdatePanel1"]//div[@class="panel panel-default"]//div[@class="table-responsive"]//table[@class="table table-hover table-heatmap"]//tr//td/span'
+
+        for sel in response.xpath(tableheader):
+            #country = sel.xpath('@href').get()
+            country = sel.xpath('normalize-space(text())').get()
             print(country)
 
-        for sel in response.xpath('//div[@id="ctl00_ContentPlaceHolder1_ctl02_UpdatePanel1"]//div[@class="panel panel-default"]//div[@class="table-responsive"]//table[@class="table table-hover table-heatmap"]//tr//td/span'):
-            data = sel.xpath('text()').get() 
+        for sel in response.xpath(tablevalues):
+            data = sel.xpath('text()').get()
             print("dados: "+data)
-           
-
-
